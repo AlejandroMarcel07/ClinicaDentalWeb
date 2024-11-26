b9ee6d19cdc0e0d9ab448d74ec956f83b7f0451d
marcel <alejocrak321zuniga@gmail.com>
2024-11-26 16:25:22 -0600

    Creacion de variables de entorno(.env)

--- config/settings.py
+++ config/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-a-q=#3j_-876kt)tzz@ldn-l3izsf**&i=x8)67)37@q7oi2lh
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
 
 
 # Application definition
@@ -58,6 +58,7 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'config.urls'
+APPEND_SLASH = True
 
 TEMPLATES = [
     {
@@ -81,11 +82,14 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+from decouple import config
+
+
 DATABASES = {
     'default': {
         'ENGINE': 'mssql',
-        'NAME': 'BD_Densy',
-        'HOST': r'DESKTOP-CVLKAEV\MSSQL2024',
+        'NAME': config('DB_NAME'),
+        'HOST': config('DB_HOST'),
         'OPTIONS': {
             'driver': 'ODBC Driver 17 for SQL Server',
             'extra_params': 'TrustServerCertificate=yes',
@@ -93,6 +97,40 @@ DATABASES = {
     }
 }
 
+from config.utils.logging_config import ANSIColorFormatter
+
+# Configura los detalles de conexión a Papertrail
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'custom_format': {
+            '()': ANSIColorFormatter,
+            'format': '%(asctime)s | %(levelname)s | %(name)s | %(message)s',
+            'datefmt': '%Y-%m-%d %H:%M:%S',  # Formato de fecha y hora
+        },
+        'verbose': {
+            'format': '{levelname} {asctime} {module} {message}',
+            'style': '{',
+        },
+    },
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+            'formatter': 'custom_format',  # Usa el formato personalizado
+        },
+        'papertrail': {
+            'level': 'INFO',
+            'class': 'logging.handlers.SysLogHandler',
+            'formatter': 'custom_format',  # Usa el formato personalizado para Papertrail
+            'address': (config('HOST_PAPERTRAIL'), int(config('PORT_PAPERTRAIL'))),
+        },
+    },
+    'root': {
+        'handlers': ['console', 'papertrail'],
+        'level': 'DEBUG',
+    },
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
@@ -129,6 +167,8 @@ SIMPLE_JWT = {
     'ALGORITHM': 'HS256',
     'SIGNING_KEY': SECRET_KEY,
 }
+
+
 
 
 # Internationalization
